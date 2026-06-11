a4b4e3d5262819b7963849ed067429b05f8de51d
Tomás Rau <62252283+tomasrau@users.noreply.github.com>
2026-06-11 11:03:43 -0300

    Update quotes-config.js

--- assets/js/quotes-config.js
+++ assets/js/quotes-config.js
@@ -2,8 +2,8 @@ window.RIO_QUOTES_CONFIG = {
     manualQuotes: {
       blue: {
         label: "Blue RIO",
-        compra: 1440,
-        venta: 1470
+        compra: 1420,
+        venta: 1450
       }
     }
   };
