4a7f55217ea308a77106d5e8821f9b84f1a2637b
Tomás Rau <62252283+tomasrau@users.noreply.github.com>
2026-06-18 08:41:58 -0300

    Update compra and venta values for Blue RIO

--- assets/js/quotes-config.js
+++ assets/js/quotes-config.js
@@ -2,8 +2,8 @@ window.RIO_QUOTES_CONFIG = {
     manualQuotes: {
       blue: {
         label: "Blue RIO",
-        compra: 1425,
-        venta: 1455
+        compra: 1450,
+        venta: 1480
       }
     }
   };
